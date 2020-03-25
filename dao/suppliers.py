615bd06447750a243cfc4ef90f2f2c76b9dcdde4
rexkater <rex.reyes@upr.edu>
2020-03-25 16:19:24 -0400

    Supplier's DAO created. Requires testing.

--- dao/suppliers.py
+++ dao/suppliers.py
@@ -4,9 +4,10 @@ import psycopg2
 class SupplierDAO:
     def __init__(self):
 
-        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
+        connection_url = "dbname=%s user=%s password=%s host=%s" % (pg_config['dbname'],
                                                             pg_config['user'],
-                                                            pg_config['passwd'])
+                                                            pg_config['passwd'],
+                                                            pg_config['host'])
         self.conn = psycopg2._connect(connection_url)
 
     def getAllSupplier(self):
