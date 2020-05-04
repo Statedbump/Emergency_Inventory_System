8cf08dfb5b108537d5b1e98a93e69e2ba80682db
yetsielaviles <yetsiel.aviles@upr.edu>
2020-05-03 21:57:55 -0400

    Hosted DB at Heroku. Fixed supplier dao and handler.

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,6 +1,12 @@
-pg_config = {
+"""pg_config = {
     'user' : 'projectuser',
     'passwd' : 'icom5016',
     'dbname' : 'eridb',
     'host' : 'localhost'
+}"""
+pg_config = {
+    'user' : 'djxaqudhuoodnk',
+    'passwd' : '4c981d57b20db4ad50339e1b5121cd03b0b5c6f2d18e67ede1877027e25e2c3c',
+    'dbname' : 'd7d9hgkosa7fho',
+    'host' : 'ec2-3-223-21-106.compute-1.amazonaws.com'
 }
--- dao/supplier.py
+++ dao/supplier.py
@@ -3,11 +3,8 @@ import psycopg2
 
 class SupplierDAO:
     def __init__(self):
-        connection_url = "dbname=%s user=%s password=%s host=%s" % (pg_config['dbname'],
-                                                                    pg_config['user'],
-                                                                    pg_config['passwd'],
-                                                                    pg_config['host'])
-        self.conn = psycopg2._connect(connection_url)
+        DATABASE_URL = 'postgres://djxaqudhuoodnk:4c981d57b20db4ad50339e1b5121cd03b0b5c6f2d18e67ede1877027e25e2c3c@ec2-3-223-21-106.compute-1.amazonaws.com:5432/d7d9hgkosa7fho'
+        self.conn = psycopg2.connect(DATABASE_URL)
 
     def getAllSuppliers(self):
         cursor = self.conn.cursor()
@@ -20,7 +17,7 @@ class SupplierDAO:
 
     def getSupplierById(self, supplier_id):
         cursor = self.conn.cursor()
-        query = "select * from supplier where supplier_id = %s;"
+        query = "select * from supplier where s_id = %s;"
         cursor.execute(query, (supplier_id,))
         result = cursor.fetchone()
         return result
@@ -28,19 +25,19 @@ class SupplierDAO:
     def getResourcesBySupplierId(self, supplier_id):
         cursor = self.conn.cursor()
         result = []
-        query = "select supplier_id, first_name, middle_initial, last_name, r_id, r_type, water_type, measurement_unit, r_location, supply_date from resource natural inner join supplier natural inner join supplies natural inner join water where supplier_id =%s and r_type ='Water';"
+        query = "select s_id, first_name, middle_initial, last_name, r_id, r_type, water_type, measurement_unit, r_location, supply_date from resource natural inner join supplier natural inner join supplies natural inner join water where s_id =%s and r_type ='Water';"
         cursor.execute(query, (supplier_id,))
         for row in cursor:
             result.append(row)
-        query = "select supplier_id, first_name, middle_initial, last_name, r_id, r_type, fuel_type, fuel_octane_rating, r_location, supply_date from resource natural inner join supplier natural inner join supplies natural inner join fuel where supplier_id =%s and r_type ='Fuel';"
+        query = "select s_id, first_name, middle_initial, last_name, r_id, r_type, fuel_type, fuel_octane_rating, r_location, supply_date from resource natural inner join supplier natural inner join supplies natural inner join fuel where s_id =%s and r_type ='Fuel';"
         cursor.execute(query, (supplier_id,))
         for row in cursor:
             result.append(row)
-        query = "select supplier_id, first_name, middle_initial, last_name, r_id, r_type, food_type, r_location, supply_date from resource natural inner join supplier natural inner join supplies natural join food where supplier_id =%s and r_type ='Food';"
+        query = "select s_id, first_name, middle_initial, last_name, r_id, r_type, food_type, r_location, supply_date from resource natural inner join supplier natural inner join supplies natural join food where s_id =%s and r_type ='Food';"
         cursor.execute(query, (supplier_id,))
         for row in cursor:
             result.append(row)
-        query = "select supplier_id, first_name, middle_initial, last_name, r_id, r_type, r_location, supply_date from resource natural inner join supplier natural inner join supplies where supplier_id =%s and r_type <> 'Water' and r_type <> 'Fuel' and r_type <>'Food';"
+        query = "select s_id, first_name, middle_initial, last_name, r_id, r_type, r_location, supply_date from resource natural inner join supplier natural inner join supplies where s_id =%s and r_type <> 'Water' and r_type <> 'Fuel' and r_type <>'Food';"
         cursor.execute(query, (supplier_id,))
         for row in cursor:
             result.append(row)
@@ -57,7 +54,7 @@ class SupplierDAO:
 
     def insert(self, first_name, middle_initial, last_name, company_name, warehouse_address, supplier_location, phone, login_id):
         cursor = self.conn.cursor()
-        query = "insert into supplier(first_name, middle_initial, last_name, company_name, warehouse_address, supplier_location, phone, login_id) values (%s, %s, %s, %s, %s, %s,%s, %s) returning supplier_id;"
+        query = "insert into supplier(first_name, middle_initial, last_name, company_name, warehouse_address, supplier_location, phone, login_id) values (%s, %s, %s, %s, %s, %s,%s, %s) returning s_id;"
         cursor.execute(query, (first_name, middle_initial, last_name, company_name, warehouse_address, supplier_location, phone, login_id))
         s_id = cursor.fetchone()[0]
         self.conn.commit()
@@ -65,14 +62,14 @@ class SupplierDAO:
 
     def delete(self, s_id):
         cursor = self.conn.cursor()
-        query = "delete from supplier where supplier_id = %s;"
+        query = "delete from supplier where s_id = %s;"
         cursor.execute(query, (s_id,))
         self.conn.commit()
         return s_id
 
     def update(self, s_id, first_name, middle_initial, last_name, company_name, warehouse_address, supplier_location, phone, login_id):
         cursor = self.conn.cursor()
-        query = "update supplier set first_name = %s, middle_initial = %s, last_name = %s, company_name = %s, warehouse_address = %s, supplier_location = %s, phone = %s, login_id = %s where supplier_id = %s;"
+        query = "update supplier set first_name = %s, middle_initial = %s, last_name = %s, company_name = %s, warehouse_address = %s, supplier_location = %s, phone = %s, login_id = %s where s_id = %s;"
         cursor.execute(query, (first_name, middle_initial, last_name, company_name, warehouse_address, supplier_location, phone, login_id, s_id,))
         self.conn.commit()
         return s_id
